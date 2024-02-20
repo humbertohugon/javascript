8c6b3b09d8144cfe569c2807b6ca861fe5151385
humbertohugon <humbertohugoneves@gmail.com>
2024-02-20 17:08:40 -0300

    ex

---
+++ exercicios/aula12/ex011.js
@@ -0,0 +1,10 @@
+var idade = 10
+if (idade >= 18) {
+    console.log('Você é maior de idade!')
+} else {
+    if (idade >= 16) {
+    console.log('Você é menor de idade, mas pode votar!')
+    } else {
+    console.log('Você é menor de idade, ainda não pode votar!')
+    }
+}
