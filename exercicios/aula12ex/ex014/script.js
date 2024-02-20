0c915e62fde40436686492da320fa0dd78ac2470
humbertohugon <humbertohugoneves@gmail.com>
2024-02-20 17:58:42 -0300

    feito

--- exercicios/aula12ex/ex014/script.js
+++ exercicios/aula12ex/ex014/script.js
@@ -1,1 +1,23 @@
-alert('Olá!')
+function carregar() {
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('imagem')
+    var data = new Date()
+    var hora = data.getHours()
+    msg.innerHTML = `Agora são ${hora} horas.`
+    if (hora >= 0 && hora < 12) {
+        // Bom dia
+        img.src = 'fotomanha.jpg'
+        document.body.style.background = '#FCE48E'
+    } else {
+        if (hora >= 12 && hora < 18) {
+            // Boa Tarde
+            img.src = 'fototarde.jpg'
+            document.body.style.background = '#9E6749'
+        } else {
+            // Boa noite
+            img.src = 'fotonoite.jpg'
+            document.body.style.background = '#8264A0'
+        }
+    }
+}
+
